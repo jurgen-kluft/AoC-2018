fad3bd0114f9c10bba1f4ddc075a4447d5e33537
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-12-08 13:33:39 +0800

    Initial Commit

---
+++ AdventOfCode-2018/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode-2018
+//
+//  Created by Jurgen Kluft on 2018/12/8.
+//  Copyright © 2018 Jurgen Kluft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
