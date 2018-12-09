8f55a2cc25a6b5948b2da5e5cc29b7e6ce045458
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-12-09 21:18:01 +0800

    + day9

--- AdventOfCode-2018/main.swift
+++ AdventOfCode-2018/main.swift
@@ -10,7 +10,7 @@ import Foundation
 
 print("Advent of Code 2018, using Swift!")
 
-var day : Int = 5
+var day : Int = 9
 
 switch (day) {
 case 1 : print("done already")
@@ -18,6 +18,7 @@ case 2 : aoc_day2()
 case 3 : aoc_day3()
 case 4 : aoc_day4()
 case 5 : aoc_day5()
+case 9 : aoc_day9()
 default: print("This day hasn't been done yet")
 }
 
